467397f8b74df5e65b3e9714e4f7b210a97d820e
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2023-12-06 01:01:50 +0530

    binary-search

---
+++ Number/palindrom.java
@@ -0,0 +1,21 @@
+package Number;
+
+public class palindrom {
+    public static void main(String[] args) {
+        // Given a number N, you need to check whether the given number is Palindrome or not. A number is said to be Palindrome when it reads the same from backward as forward.
+        int n = 121;
+        int org = n;
+        int res =0;
+        while(n>0){
+            int mod = n%10;
+            res = res*10 + mod;
+            n=n/10;
+        }
+        if(res == org){
+            System.out.print(true);
+        }else{
+            System.out.print(false);
+            
+        }
+    }
+}
