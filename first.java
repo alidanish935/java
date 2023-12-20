ad6d641df8389e922ede4a02756746a1c833366d
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2023-12-21 01:33:38 +0530

    try

--- first.java
+++ first.java
@@ -2,33 +2,37 @@ import java.util.HashMap;
 
 class first{
     public static void main(String[] args) {
-        int arr[]={1,4,20,8,10,5};
-        int n = arr.length;
-        int sum = 32;
+        // int arr[]={1,4,20,8,10,5};
+        // int n = arr.length;
+        // int sum = 32;
 
-        int start = 0, end =-1,curr_sum =0;
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        // int start = 0, end =-1,curr_sum =0;
+        // HashMap<Integer,Integer> hm = new HashMap<>();
 
-        for(int i=0;i<n;i++){//0
-            curr_sum += arr[i];//1
-            if (curr_sum == sum) {
-                end = i;
-            }
-            if (hm.containsKey(curr_sum-sum)) {
-                start = hm.get(curr_sum - sum)+1;
-                end = i;
-            }
-            hm.put(curr_sum,i);
-        }
-        if(end == -1){
+        // for(int i=0;i<n;i++){//0
+        //     curr_sum += arr[i];//1
+        //     if (curr_sum == sum) {
+        //         end = i;
+        //     }
+        //     if (hm.containsKey(curr_sum-sum)) {
+        //         start = hm.get(curr_sum - sum)+1;
+        //         end = i;
+        //     }
+        //     hm.put(curr_sum,i);
+        // }
+        // if(end == -1){
 
-            System.out.println(sum+" is not found  ");
-        }else{
+        //     System.out.println(sum+" is not found  ");
+        // }else{
 
-            System.out.println("Sub arr found between "+start +" to "+end);
-        }
+        //     System.out.println("Sub arr found between "+start +" to "+end);
+        // }
 
-        
+        double a =20,b=3;
+        double c = a/b;
+        System.out.println(c);
+        // double a = 6.3;
+        System.out.println(Math.ceil(a));
        
     }
 }
