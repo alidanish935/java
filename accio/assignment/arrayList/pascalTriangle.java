0d9e7061e041ccde71ea5ff473adedc8b51251f5
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2023-12-30 13:29:46 +0530

    str

---
+++ accio/assignment/arrayList/pascalTriangle.java
@@ -0,0 +1,7 @@
+package accio.assignment.arrayList;
+
+public class pascalTriangle {
+    public static void main(String[] args) {
+        //link  ->  https://course.acciojob.com/idle?question=12f0a018-3366-4117-a52d-4baec8223a21
+    }
+}
---
+++ accio/assignment/string/distinctPalindromicStr.java
@@ -0,0 +1,7 @@
+package accio.assignment.string;
+
+public class distinctPalindromicStr {
+    public static void main(String[] args) {
+        // link  -> https://course.acciojob.com/idle?question=976cb4c0-3fca-47df-8cd3-59c58c474b05
+    }
+}
