54abdaabd1db9a239d3db867a471ff146d03d40d
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-01-06 22:22:57 +0530

    kaden's

---
+++ accio/contest/janruary/module1.java
@@ -0,0 +1,12 @@
+package accio.contest.janruary;
+
+public class module1 {
+    public static void main(String[] args) {
+        // Alice's Game ->  https://course.acciojob.com/idle?question=29d70f12-0b5f-43b2-b9d8-f6d90864a588
+        // Alice's2 Game -> https://course.acciojob.com/idle?question=dd6d680a-dcc7-46f8-827f-3a905db03276
+        // Z traversal  -> https://course.acciojob.com/idle?question=7dfcf9d3-cd2e-4f9d-b03e-c6a9b7648669
+        // N traversal  -> https://course.acciojob.com/idle?question=ee1eb3c8-e5fe-4d9e-84a1-43cb659d783a
+        // Dispute ->      https://course.acciojob.com/idle?question=ce806649-c0ff-4fa2-abce-384543a4ed7b
+        // friendship ->   https://course.acciojob.com/idle?question=5c430981-6c78-4c1a-afff-aa783a6cc8dc
+    }
+}
--- first.java
+++ first.java
@@ -34,15 +34,52 @@ class first{
         // System.out.println(c);
         // // double a = 6.3;
         // System.out.println(Math.ceil(a));
-        String s = "my name is danish";
-        String[] arr=s.split(" ");
-        // char arr[]=new char[s.length()];
-        // for(int i=0;i<s.length();i++){
-        //     String str = "";
-        //     arr[i]=s.charAt(i);
-        //     System.out.print(arr[i]);
+        // String s = "my name is danish";
+        // String[] arr=s.split(" ");
+        // // char arr[]=new char[s.length()];
+        // // for(int i=0;i<s.length();i++){
+        // //     String str = "";
+        // //     arr[i]=s.charAt(i);
+        // //     System.out.print(arr[i]);
+        // // }
+        // System.out.println(Arrays.toString(s.split(" ")));
+
+
+        // int arr[]={-1,-8,8,-9,6,-3,8};
+        // int n = arr.length;
+
+        // int sum =0;int max = Integer.MIN_VALUE;
+        // for(int i=0;i<n;i++){//1
+        //     sum = sum +arr[i];//-1 -8
+        //     if(sum>=max){//-8>=-1
+        //         max = sum;//-1
+        //     }
+        //     if(sum<0){//
+        //         sum=0;//0
+        //     }
         // }
-        System.out.println(Arrays.toString(s.split(" ")));
+        // System.out.println(max);
+
+
+        // Binary search
+        int arr[]={2,3,4,5,6,7,8,9};// array must be sorted for binary search
+        int n = arr.length;int k = 3;
+         
+        int st =0;int ed = n-1;
+        while(st<=ed){
+            int mid = (st+ed)/2;
+            
+            if(k == arr[mid]){
+                System.out.println("key found at "+mid);
+                return;
+            }
+            if(k<arr[mid]){
+                ed = mid-1;
+            }
+            if(k>arr[mid]){
+                st = mid+1;
+            }
+        }
        
     }
 }
